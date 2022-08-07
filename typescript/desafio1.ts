03efd65e8388cc255e2e2fc4fd7dc8bb1417de58
Rodrigo J. Moraes <108589662+devrodrigomoraes@users.noreply.github.com>
2022-08-07 13:16:46 -0300

    Add files via upload

---
+++ typescript/desafio1.ts
@@ -0,0 +1,23 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+/*let employee = {};
+employee.code = 10;
+employee.name = "John";*/
+
+
+interface Employee {
+    code: number,
+    name: string;
+}
+
+let employee1: Employee = {
+    code: 2,
+    name: "John",
+}
+
+let employee2 = {} as Employee;
+employee2.code = 3;
+employee2.name = "John";
+
+console.log(`Código ${employee1.code} e o nome ${employee1.name} \n`)
+console.log(`Código ${employee2.code} e o nome ${employee2.name} \n`);
